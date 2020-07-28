9641de3d2260655215c5648b53543a68fc48ef0f
lintechao <lintechao@yingzi.com>
2020-07-28 17:40:43 +0800

    dingtalk debug

--- API/views.py
+++ API/views.py
@@ -96,12 +96,12 @@ def ding_vuln_list(request):
     if not token:
         return permission_denied(request)
     user_name_zh = JWT.decode_jwt(token).get('user')
-    if not user_name_zh:
-        return page_not_found(request)
+    # if not user_name_zh:
+    #     return page_not_found(request)
     page = request.POST.get('page')
     rows = request.POST.get('limit')
-    user_name = han_to_pinyin(user_name_zh)
-    res = get_user_area(user_name)
+    # user_name = han_to_pinyin(user_name_zh)
+    # res = get_user_area(user_name)
     # is_admin, user_area_list = res.get('is_admin'), res.get('user_area_list')
     is_admin = True  # 调试用
     user_area_list = None
