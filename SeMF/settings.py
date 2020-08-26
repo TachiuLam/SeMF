b9f0b12c39a72033fdd9e5f074891bc039b88dc3
lintechao <lintechao@yingzi.com>
2020-08-26 17:40:54 +0800

    change redis options IGNORE_EXCEPTIONS

--- SeMF/settings.py
+++ SeMF/settings.py
@@ -179,9 +179,9 @@ CACHES = {
             "CONNECTION_POOL_KWARGS": {"max_connections": 10000},
             "SOCKET_CONNECT_TIMEOUT": 5,  # 建立连接超时 in seconds
             "SOCKET_TIMEOUT": 5,  # 连接建立后读写超时 in seconds
-            # "IGNORE_EXCEPTIONS": True,  # 忽略异常
+            "IGNORE_EXCEPTIONS": True,  # 忽略异常
             "PASSWORD": "1qaz@WSX",
-            "DECODE_RESPONSES": True
+            "DECODE_RESPONSES": True    # 数据存储格式为字符串
         }
     },
 }
