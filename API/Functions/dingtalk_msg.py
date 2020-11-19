34b86c62aee3bd00c1ca70c15ae0cdff35f11c69
lintechao <lintechao@yingzi.com>
2020-11-19 17:38:35 +0800

    modified nat alert method

--- API/Functions/dingtalk_msg.py
+++ API/Functions/dingtalk_msg.py
@@ -43,11 +43,24 @@ class DingTalkMsg:
 
     @staticmethod
     def card_msg(message):
+        text = '### ' + message.get('tittle') + '\n'
+        text = text + "##### " + message.get('content') + str(datetime.date.today())
         msg = {
             "msgtype": "action_card",
             "action_card": {
                 "title": message.get('tittle'),
-                "markdown": message.get('content'),
+                "markdown": text,
+                "btn_orientation": "0",
+                "btn_json_list": [
+                    # {
+                    #     "title": "受理",
+                    #     "action_url": "http://127.0.0.1/vuln/user/details/"
+                    # },
+                    {
+                        "title": "添加白名单",
+                        "action_url": 'https://semf.yingzi.com'
+                    },
+                ]
             }
         }
         return msg
