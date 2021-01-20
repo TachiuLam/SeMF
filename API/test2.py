df39938229fee8d7f9b8ac0b547ca3fa56e49464
lintechao <lintechao@yingzi.com>
2021-01-20 10:05:17 +0800

    add web report upload method

--- API/test2.py
+++ API/test2.py
@@ -3,7 +3,8 @@
 # lintechao@yingzi.com
 # 2020/9/15 11:53
 import pandas as pd
-from MappedManage.models import Mapped
+import re
+# from MappedManage.models import Mapped
 
 
 
@@ -21,6 +22,13 @@ if __name__=='__main__':
     # for each in filepath:
     #     report_main(each)
     ip = '172.18.20.200 '
-    mip = Mapped.objects.filter(LANip__asset_key__icontains=ip)
-    if mip.exists():
-        print('ip 存在')
+    # mip = Mapped.objects.filter(LANip__asset_key__icontains=ip)
+    # if mip.exists():
+    #     print('ip 存在')
+    if re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+                "223.168.1.1"):
+        print(11)
+    level = 2
+    level_info = {"信息": "0", "低危": "1", "中危": "2", "高危": "3", "紧急": "4"}
+    level = level_info.get(level) if level_info.get(level) else "0"
+    print(level)
